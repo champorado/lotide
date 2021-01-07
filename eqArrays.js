c9a66bd7fe2311ccc273514cdb2b60a356c64b66
Ricka Ortiz <rickairis.ortiz@gmail.com>
2021-01-07 06:42:17 +0000

    second commit - solved

--- eqArrays.js
+++ eqArrays.js
@@ -30,5 +30,5 @@ const eqArrays = function(array1, array2) {
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays([1], ["cat", "dog"]), false);
+assertEqual(eqArrays([], ["cat", "dog"]), false);
 assertEqual(eqArrays(["cat", "dog", "mouse"], ["cat", "dog", "mouse"]), true);
